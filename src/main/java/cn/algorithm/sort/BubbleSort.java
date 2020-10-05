b6808a9dbaab708bb7c2e1180c1ecd72c9629a38
e <13750015150@139.com>
2020-10-06 07:09:48 +0800

    提交算法和设计模式工程

---
+++ src/main/java/cn/algorithm/sort/BubbleSort.java
@@ -0,0 +1,53 @@
+package cn.algorithm.sort;
+
+/*
+冒泡和选择排序
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = {23, 45, 17, 11, 13, 89, 72, 26, 3, 17, 11, 13};
+        int[] bubble = bubble(arr);
+        System.out.println(bubble);
+        int[] select = select(arr);
+        System.out.println(select);
+
+    }
+
+    //冒泡排序的方法
+    public static int[] bubble(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+        return arr;
+    }
+
+    //选择排序
+    public static int[] select(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+
+        }
+        return arr;
+    }
+
+
+}
---
+++ src/main/java/cn/algorithm/sort/QuickSort.java
@@ -0,0 +1,38 @@
+package cn.algorithm.sort;
+
+/*
+快速排序
+ */
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] arr = {23, 45, 17, 11, 13, 89, 72, 26, 3, 17, 11, 13};
+        quick(arr, 0, arr.length-1);
+        System.out.println(arr);
+    }
+
+    public static void quick(int[] arr, int begin, int end) {
+        if (begin<end) {
+            int temp = arr[begin];
+            int i = begin;
+            int j = end;
+            while (j > i) {
+
+                while (i < j && arr[j] > temp) {
+                    j--;
+                }
+                //找到了比temp小的就把这个值放到arr[i]；
+                arr[i] = arr[j];
+                while (i < j && arr[i] <= temp) {
+                    i++;
+                }
+                arr[j] = arr[i];
+            }
+            arr[i] = temp;
+            quick(arr, begin, i - 1);
+            quick(arr, i + 1, end);
+        }else {
+            return;
+        }
+    }
+}
